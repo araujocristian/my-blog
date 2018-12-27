974edbec844bfbc31d2332ea701171fe9b2bd7c8
Cristian Araujo <araujocristian@live.com>
2018-12-26 22:45:49 -0300

    Updates

---
+++ precache-manifest.f673891b8ef379b1ccda2623b262b8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce8160378f6473d94fcf",
+    "url": "/my-blog/static/css/main.7f01804e.chunk.css"
+  },
+  {
+    "revision": "ce8160378f6473d94fcf",
+    "url": "/my-blog/static/js/main.ce816037.chunk.js"
+  },
+  {
+    "revision": "906e5b9524e457168e95",
+    "url": "/my-blog/static/css/1.a66d32b9.chunk.css"
+  },
+  {
+    "revision": "906e5b9524e457168e95",
+    "url": "/my-blog/static/js/1.906e5b95.chunk.js"
+  },
+  {
+    "revision": "c3957ea4b8d554660fff",
+    "url": "/my-blog/static/js/runtime~main.c3957ea4.js"
+  },
+  {
+    "revision": "0426493ce0ca45ddc9ecea6b652e0994",
+    "url": "/my-blog/index.html"
+  }
+];
